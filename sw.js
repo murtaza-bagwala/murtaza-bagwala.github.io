6dacc5ae16708286d5a4588688d9a3b29ab94bf5
Murtaza Bagwala <bagwalamurtaza@gmail.com>
2026-03-22 22:24:36 +0530

    deploy: publish site 2026-03-22 22:24:36

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-af78c2b0051185f2c120.js"
+    "url": "webpack-runtime-33eb2841a6b6de6d6d69.js"
   },
   {
-    "url": "styles.60657d39ecb607ad9fc2.css"
+    "url": "styles.1f2f0f481ff1f30f979d.css"
   },
   {
     "url": "framework-df177803de5cb416bb55.js"
   },
   {
-    "url": "app-dbb1a8aed6b6225f3135.js"
+    "url": "app-bcc4cd124eb23d60a5d5.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "33b66dbdcd47548e3988c3c9891a722d"
+    "revision": "05f5b8ba3f63a8543405919a9e018772"
   },
   {
     "url": "manifest.webmanifest",
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-dbb1a8aed6b6225f3135.js`))) {
+  if (!resources || !(await caches.match(`/app-bcc4cd124eb23d60a5d5.js`))) {
     return await fetch(event.request)
   }
 
